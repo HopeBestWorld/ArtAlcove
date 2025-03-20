866b375fc605998f712b6135bfd3ae93d847bb66
karen-malka <ks959@cornell.edu>
2025-03-20 12:21:42 -0400

    added more data sets

--- backend/app.py
+++ backend/app.py
@@ -22,6 +22,18 @@ with open(json_file_path, 'r', encoding='utf-8') as file:
     stretched_canvas_reviews_df = pd.DataFrame(data['stretched_canvas_reviews'])
     alcohol_markers_df = pd.DataFrame(data['alcohol_markers'])
     alcohol_markers_reviews_df = pd.DataFrame(data['alcohol_markers_reviews'])
+    colored_pencils_df = pd.DataFrame(data['colored_pencils'])
+    colored_pencils_reviews_df = pd.DataFrame(data['colored_pencils_reviews'])
+    drawing_pencils_df = pd.DataFrame(data['drawing_pencils'])
+    drawing_pencils_reviews_df = pd.DataFrame(data['drawing_pencils_reviews'])
+    graphite_df = pd.DataFrame(data['graphite'])
+    graphite_reviews_df = pd.DataFrame(data['graphite_reviews'])
+    oil_pastels_df = pd.DataFrame(data['oil_pastels'])
+    oil_pastels_reviews_df = pd.DataFrame(data['oil_pastels_reviews'])
+    pastel_pencils_df = pd.DataFrame(data['pastel_pencils'])
+    pastel_pencils_reviews_df = pd.DataFrame(data['pastel_pencils_reviews'])
+    soft_pastels_df = pd.DataFrame(data['soft_pastels'])
+    soft_pastels_reviews_df = pd.DataFrame(data['soft_pastels_reviews'])
 
 app = Flask(__name__)
 CORS(app)
@@ -32,6 +44,19 @@ def json_search(query):
     merged_df = pd.merge(stretched_canvas_df, stretched_canvas_reviews_df, left_on='product', right_on='product', how='inner')
     merged_df2 = pd.merge(alcohol_markers_df, alcohol_markers_reviews_df, left_on='product', right_on='product', how='inner')
     merged_df = pd.concat([merged_df, merged_df2])
+    merged_df2 = pd.merge(colored_pencils_df, colored_pencils_reviews_df, left_on='product', right_on='product', how='inner')
+    merged_df = pd.concat([merged_df, merged_df2])
+    merged_df2 = pd.merge(drawing_pencils_df, drawing_pencils_reviews_df, left_on='product', right_on='product', how='inner')
+    merged_df = pd.concat([merged_df, merged_df2])
+    merged_df2 = pd.merge(graphite_df, graphite_reviews_df, left_on='product', right_on='product', how='inner')
+    merged_df = pd.concat([merged_df, merged_df2])
+    merged_df2 = pd.merge(oil_pastels_df, oil_pastels_reviews_df, left_on='product', right_on='product', how='inner')
+    merged_df = pd.concat([merged_df, merged_df2])
+    merged_df2 = pd.merge(pastel_pencils_df, pastel_pencils_reviews_df, left_on='product', right_on='product', how='inner')
+    merged_df = pd.concat([merged_df, merged_df2])
+    merged_df2 = pd.merge(soft_pastels_df, soft_pastels_reviews_df, left_on='product', right_on='product', how='inner')
+    merged_df = pd.concat([merged_df, merged_df2])
+
     matches = merged_df.groupby(
         ['product', 'siteurl', 'price', 'rating', 'imgurl', 'descr']
     ).agg({
--- init.sql
+++ init.sql
@@ -43,3 +43,117 @@ CREATE TABLE alcohol_markers_reviews(
     product varchar(250),
     FOREIGN KEY(product) REFERENCES alcohol_markers(product)
 );
+
+CREATE TABLE colored_pencils(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    product varchar(250),
+    descr varchar(1024),
+    siteurl varchar(200) UNIQUE,
+    price dec(6, 2),
+    rating dec(2, 1),
+    imgurl varchar(200),
+    price_range varchar(50)
+);
+
+CREATE TABLE colored_pencils_reviews(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    review_title varchar(200),
+    review_desc varchar(1024),
+    product varchar(250),
+    FOREIGN KEY(product) REFERENCES colored_pencils(product)
+);
+
+CREATE TABLE drawing_pencils(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    product varchar(250),
+    descr varchar(1024),
+    siteurl varchar(200) UNIQUE,
+    price dec(6, 2),
+    rating dec(2, 1),
+    imgurl varchar(200),
+    price_range varchar(50)
+);
+
+CREATE TABLE drawing_pencils_reviews(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    review_title varchar(200),
+    review_desc varchar(1024),
+    product varchar(250),
+    FOREIGN KEY(product) REFERENCES drawing_pencils(product)
+);
+
+CREATE TABLE graphite(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    product varchar(250),
+    descr varchar(1024),
+    siteurl varchar(200) UNIQUE,
+    price dec(6, 2),
+    rating dec(2, 1),
+    imgurl varchar(200),
+    price_range varchar(50)
+);
+
+CREATE TABLE graphite_reviews(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    review_title varchar(200),
+    review_desc varchar(1024),
+    product varchar(250),
+    FOREIGN KEY(product) REFERENCES graphite(product)
+);
+
+CREATE TABLE oil_pastels(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    product varchar(250),
+    descr varchar(1024),
+    siteurl varchar(200) UNIQUE,
+    price dec(6, 2),
+    rating dec(2, 1),
+    imgurl varchar(200),
+    price_range varchar(50)
+);
+
+CREATE TABLE oil_pastels_reviews(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    review_title varchar(200),
+    review_desc varchar(1024),
+    product varchar(250),
+    FOREIGN KEY(product) REFERENCES oil_pastels(product)
+);
+
+CREATE TABLE pastel_pencils(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    product varchar(250),
+    descr varchar(1024),
+    siteurl varchar(200) UNIQUE,
+    price dec(6, 2),
+    rating dec(2, 1),
+    imgurl varchar(200),
+    price_range varchar(50)
+);
+
+CREATE TABLE pastel_pencils_reviews(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    review_title varchar(200),
+    review_desc varchar(1024),
+    product varchar(250),
+    FOREIGN KEY(product) REFERENCES pastel_pencils(product)
+);
+
+CREATE TABLE soft_pastels(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    product varchar(250),
+    descr varchar(1024),
+    siteurl varchar(200) UNIQUE,
+    price dec(6, 2),
+    rating dec(2, 1),
+    imgurl varchar(200),
+    price_range varchar(50)
+);
+
+CREATE TABLE soft_pastels_reviews(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    review_title varchar(200),
+    review_desc varchar(1024),
+    product varchar(250),
+    FOREIGN KEY(product) REFERENCES soft_pastels(product)
+);
